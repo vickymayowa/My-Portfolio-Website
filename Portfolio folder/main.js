4c98158a8502093568ee90f14a43f33b0de706ee
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-03-10 13:20:41 +0100

    Changed my backend rootdirectory

---
+++ Portfolio folder/main.js
@@ -0,0 +1,39 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navbarDark');
+
+//     }
+//     else {
+//         header.classList.remove('navbarDark');
+//     }
+// }
+
+document.getElementById("Contact").addEventListener("click",()=>{
+    setTimeout(() => {
+        disp.innerHTML = "Thank u for contacting vicky website.Your Message has been sent"
+    },1000);
+})
+
+var typed = new Typed(".typing",{
+    strings: ["","Web Developer", "", "Video Editor", "", "Graphic Designer", "", "Forex Trader", "", "Crypto Dealer", "Book Writer", "Business Coach"],
+    typeSpeed:600,
+    BackSpeed:40,
+    loop:true
+})
+
+const typed = select('.typed')
+if (typed) {
+ let typed_strings = typed.getAttribute('data-typed-items')
+ typed_strings = typed_strings.split(',')
+ new Typed('.typed', {
+   strings: typed_strings,
+   loop: true,
+   typeSpeed: 100,
+   backSpeed: 50,
+   backDelay: 2000
+ });
+}
