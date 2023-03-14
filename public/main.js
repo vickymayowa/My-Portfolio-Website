8f15596a138fa1de624753d3c2207441cd145153
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-03-14 16:48:07 +0100

    Created Another Date Refernce

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+//const header = document.querySelector('.navbar');
 
 // window.onscroll = function() {
 //     var top = window.scrollY;
