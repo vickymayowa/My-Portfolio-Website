9628f4e0277a5f0eeb08c1a9611c808dee88bbb5
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-03-14 14:02:15 +0100

    Still working on the frontend file

--- public/main.js
+++ public/main.js
@@ -12,11 +12,11 @@ const header = document.querySelector('.navbar');
 //     }
 // }
 
-document.getElementById("Contact").addEventListener("click",()=>{
-    setTimeout(() => {
-        disp.innerHTML = "Thank u for contacting vicky website.Your Message has been sent"
-    },1000);
-})
+// document.getElementById("Contact").addEventListener("click",()=>{
+//     setTimeout(() => {
+//         disp.innerHTML = "Thank u for contacting vicky website.Your Message has been sent"
+//     },1000);
+// })
 
 // var typed = new Typed(".typing",{
 //     strings: ["","Web Developer", "", "Video Editor", "", "Graphic Designer", "", "Forex Trader", "", "Crypto Dealer", "Book Writer", "Business Coach"],
